cbc0c4dbd19b68ad4b72e0dce9cd57363d8d0ee9
cedric-winbush <cawinbushjr@gmail.com>
2020-12-28 16:44:39 -0600

    adds form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from "./components/Details/Details";
 import useStyles from './styles';
+import Main from "./components/Main/Main";
 
 const App = () => {
 
@@ -21,8 +22,8 @@ const App = () => {
         <Grid item xs={12} sm={4}>
           <Details title ="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          Main{" "}
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
           <Details title = "Expense"/>
